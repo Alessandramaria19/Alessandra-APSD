fe753f3b82a6ce504bba4f216e92b3f6820f248f
Alessandramaria19 <alessandra.ninacs19@gmail.com>
2025-10-19 12:40:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,17 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print ('Buna')
-print('hei')
+    import string
+    x="Potrivit ABC Science, la apus și la răsărit calea parcursă de razele solare prin atmosferă este cea mai lungă. Prin urmare, culorile care reușesc să ajungă mai ușor la noi sunt roșul și portocaliul, în timp ce albastrul și violetul se pierd din cauza difuziei."
+    lung=len(x)
+    mj=lung//2
+    jum1=x[:mj]
+    jum2=x[mj:]
+    jum1=jum1.upper()
+    jum1=jum1.strip()
+    jum2=jum2[::-1]
+    jum2=jum2.capitalize()
+    set= str.maketrans('' ,'', string.punctuation)
+    jum2=jum2.translate(set)
+    print(jum1+jum2)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
