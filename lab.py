a048a5293a6778563d9ea5580cb42c788176a158
Alessandramaria19 <alessandra.ninacs19@gmail.com>
2025-10-21 13:21:13 +0300

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,31 @@
+numar1 = 10
+numar2 = 7
+if numar1 > numar2:
+    print("Numarul 1 este mai mare decat numarul 2")
+elif numar1 < numar2:
+    print("Numarul 1 este mai mic decat numarul 2")
+else:
+    print("Numarul 1 este egal cu numarul 2")
+
+sir = "radar"
+if sir == sir[::-1]:
+     print("Sirul e palindrom")
+else:
+     print("Sirul nu e paindrom")
+
+        # in -functie ce verifica daca exista in sir
+
+            #for element in colectie
+            #bloc de cod
+            #range (start,stop, pas)
+            #pas cu cat crestem
+for i in range(0,10,1):
+    print(i, end=" ")
+    # for i in range(10) are automat pasul 1 si porneste de la 0
+sir ="Hello world"
+for caracter in sir:
+    print(caracter)
+    print("*")
+
+for i in range(len(sir)):
+    print(i,sir[i])
